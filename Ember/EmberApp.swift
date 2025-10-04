5e5559493350cce9a8232d1d612cba972af4ce23
edlng <edwardliangedli@gmail.com>
2025-10-04 11:35:38 -0700

    skeleton

---
+++ Ember/EmberApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmberApp.swift
+//  Ember
+//
+//  Created by Edward Liang on 2025-10-04.
+//
+
+import SwiftUI
+
+@main
+struct EmberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmberTests/EmberTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmberTests.swift
+//  EmberTests
+//
+//  Created by Edward Liang on 2025-10-04.
+//
+
+import Testing
+@testable import Ember
+
+struct EmberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
